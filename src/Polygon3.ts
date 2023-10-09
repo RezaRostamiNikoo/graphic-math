160ea8babd8d00014fbf9a5cf38203b0bc3de459
Reza Rostaminikoo RezaRostaminikoo@gmail.com <RezaRostaminikoo@gmail.com>
2023-10-09 15:10:31 -0400

    add polygon3 and its test file

---
+++ src/Polygon3.ts
@@ -0,0 +1,72 @@
+import { Mat4, Vec3 } from "./";
+import { CircularLinkedList } from "./CircularLinkedList";
+
+export default class Polygon3 {
+    private _points: CircularLinkedList<Vec3>
+
+    constructor(points: Array<Vec3>) {
+        // TODO: check if next check is necessary 
+        if (points.length < 3) throw new Error("Polygon3.constructor | length of points should be at lest three")
+        this._points = new CircularLinkedList(points)
+    }
+
+    /** returns the polygon's points as Array */
+    get points(): Array<Vec3> { return this._points.toArray() }
+
+    /**
+     * adds new point to the polyon
+     * @param {Vec3} point 
+     * @param {number} index index of where the point should be placed in the point list - if it is nor passed the points will be added to the end of list
+     * @returns {Polygon3}
+     */
+    add(point: Vec3, index?: number): this {
+        if (index) this._points.insertAt(point, index)
+        else this._points.add(point)
+        return this
+    }
+
+    /** clears the points */
+    clear() { this._points = new CircularLinkedList() }
+
+    /**
+     * returns a copy of current polygon
+     * @returns {Polygon3}
+     */
+    clone(): Polygon3 {
+        const points = this._points.toArray().map(v => v.clone())
+        const result = new Polygon3(points);
+        return result;
+    }
+
+    /**
+     * copys all the components of the given polygon to the current polygon
+     * @param {Polygon} polygon 
+     * @returns {Polygon3}
+     */
+    copy(polygon: Polygon3): Polygon3 {
+        this.clear()
+        const _this = this;
+        polygon.points.map(v => v.clone()).forEach(p => _this.add(p));
+        return this;
+    }
+
+    applyMat4(mat: Mat4): Polygon3 {
+        this.points.forEach(p => p.applyMat4(mat));
+        return this;
+    }
+
+    // canMakeVolumeWith(polygon: Polygon3): boolean {
+    //     if (this._points.length !== this._points.length) throw new Error(`Polygon3.canMakeVolumeWith | polygons do not have the same length of points`);
+    //     return true;
+    // }
+
+    // toMesh3(holes: Array<Polygon3> = []): Mesh3 {
+    //     return new EarcutTriangulation3().generateMesh(this._points, holes.map(h => h._points));
+    // }
+
+    toJson(): object {
+        return this.points.map(p => p.toJson());
+    }
+}
+
+export { Polygon3 }
---
+++ test/polygon.spec.ts
@@ -0,0 +1,42 @@
+import { Mat4, Vec3 } from "../src"
+import { Polygon3 } from "../src/Polygon3"
+describe("polygon", () => {
+
+
+    test('1', () => {
+
+        const v1 = new Vec3(0, 0, 0)
+        const v2 = new Vec3(2, 0, 0)
+        const v3 = new Vec3(2, 1, 0)
+
+        const p = new Polygon3([v1, v2, v3])
+
+        expect(p.toJson()).toEqual([
+            { x: 0, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 2, y: 1, z: 0 },
+        ])
+        expect(p.points.map(v => v.toArray())).toEqual([[0, 0, 0], [2, 0, 0], [2, 1, 0],])
+
+        p.add(new Vec3(0, 1, 0))
+        expect(p.toJson()).toEqual([
+            { x: 0, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 2, y: 1, z: 0 },
+            { x: 0, y: 1, z: 0 },
+        ])
+
+        const m = Mat4.makeRotationX(Math.PI / 2)
+
+        p.applyMat4(m)
+        expect(p.toJson()).toEqual([
+            { x: 0, y: 0, z: 0 },
+            { x: 2, y: 0, z: 0 },
+            { x: 2, y: 0, z: 1 },
+            { x: 0, y: 0, z: 1 },
+        ])
+
+
+    })
+
+})
